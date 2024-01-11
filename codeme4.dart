20750598e8dea27251c9904b6804f847a86e8f1a
Niroop Cv <141113159+Niroop-Cv@users.noreply.github.com>
2024-01-11 17:32:13 +0530

    Add files via upload

---
+++ codeme4.dart
@@ -0,0 +1,89 @@
+
+
+//Types of printing patterns.dart
+
+
+// void main() {
+//   for (int i = 1; i <= 5; i++) {
+//     var star = "";
+//     for (int j = 5 - i; j >= 1; j--) {
+//       star += " ";
+//     }
+//     for (int k = 1; k <= i; k++) {
+//       star += "* ";
+//     }
+//     print(star); 
+//   } 
+
+// }
+
+//2
+import 'dart:io';
+
+// void main(){
+//    for(int i=1;i<=5;i++){
+//     for(int j=1;j<=i;j++){
+//         // print("*");  
+//         stdout.write("*");
+//     }
+// print("\n");
+//   }
+
+// }
+  
+
+  //3
+
+
+
+// void main(){
+//    for (int i = 5; i >= 1; i--) {
+//     var star = "";
+//     for (int j = i; j >= 1; j--) {
+//       star += "*";
+//     }
+   
+    
+//     print(star); 
+//   } 
+// }
+
+//4
+
+
+// void main(){
+//   for(int i=1;i<=5;i++){
+//     for(int j=1;j<=5;j++){
+//       if ((i+j)<=5) {
+//          stdout.write(" ");
+//       }
+//       else{
+//       stdout.write("*");
+//       }
+   
+//     }
+//       print("\n");
+//   }
+// }
+
+
+//5
+
+//  void main() {
+//   for (int i = 1; i <= 5; i++) {
+//     var star = "";
+//     for (int j = 5 - i; j >= 1; j--) {
+//       star += " ";
+//     }
+//     for (int k = 1; k <= i; k++) {
+//       star += "$i ";
+//     }
+//     print(star); 
+//   } 
+
+// }
+ 
+
+ 
+
+ 
